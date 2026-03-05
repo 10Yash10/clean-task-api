8a16f8ee981af5a4913438afc66db7b27a1c9bd3
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2026-03-05 18:14:21 +0530

    added all methods for interface implementation

--- app/infrastructure/database/repositories/task_repository.py
+++ app/infrastructure/database/repositories/task_repository.py
@@ -29,3 +29,74 @@ class TaskRepository(ITaskRepository):
     
     # method to get all tasks
     def get_all() -> List[Task]:
+        models = self.db.query(TaskModel).all()
+
+        tasks = []
+
+        for model in models:
+            tasks.append(
+                Task(
+                    id=model.id,
+                    title=model.title,
+                    description=model.description,
+                    status=model.status,
+                    priority=model.priority,
+                    assigned_user_id=model.assigned_user_id
+                )
+            )
+
+        return tasks
+
+    
+    # method to create a task
+    def create(self, task: Task) -> Task:
+        model = Task(
+            title=task.title,
+            description=task.description,
+            status=task.status,
+            priority=task.priority,
+            assigned_user_id=task.assinged_user_id,
+        )
+
+        self.db.add(model)
+        self.db.commit()
+        self.db.refresh(model)
+
+        return Task(
+            id = model.id,
+            title=model.title
+            description=model.description,
+            status=model.status,
+            priority=model.priority,
+            assigned_user_id=model.assigned_user_id
+        )
+
+
+    # method to update the exisiting task
+    def update(self, task:Task) -> Task:
+
+        # fetch the model from database
+        model = self.db.query(TaskModel).filter(TaskModel.id == task.id).first()
+
+        if not model:
+            return Null
+
+        # update the model with new task values
+        model.title=task.title
+        model.description=task.description
+        model.status=task.status
+        model.priority=task.priority
+        model.assigned_user_id=task.assigned_user_id
+
+        # update the database with updated model
+        self.db.commit()
+        self.db.refresh(model)
+
+        return Task(
+            id=model.id,
+            title=model.title,
+            description=model.description,
+            status=model.status,
+            priority=model.priority,
+            assigned_user_id=model.assigned_user_id
+        )
